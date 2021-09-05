84d4170d77998ad251f648b66b26918f0adb1581
tjdgml97 <parksunghee9@naver.com>
2021-09-05 21:45:02 +0900

    1st commit

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.example.lab1;
+
+import java.util.Scanner; 
+
+/**
+ * @author ssong
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello world!!!\n");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+name+ "]님 환영합니다~");
+	}
+
+}
